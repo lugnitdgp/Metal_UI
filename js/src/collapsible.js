36a6703a40d978f91ccac76d332b284ef1fd2213
Yash Vardhan <57630799+Error-200@users.noreply.github.com>
2020-02-01 00:14:50 +0530

    JS file for collapsible

---
+++ js/src/collapsible.js
@@ -0,0 +1,14 @@
+const collapse = document.getElementsByClassName("collapsible");
+
+
+for (var i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
