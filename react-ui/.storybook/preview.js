2ba19fe1443777813220e6d890c8ebdd308a57f8
Romit Karmakar <romitkarmakar@gmail.com>
2021-01-18 18:51:25 +0000

    Added storybook for tailwindui

---
+++ react-ui/.storybook/preview.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import Layout from './Layout';
+addDecorator(storyFn => <Layout>{storyFn()}</Layout>);
+addDecorator(withInfo({
+  inline: true,
+  propTablesExclude: [Layout]
+}));
